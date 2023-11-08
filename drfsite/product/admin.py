5249446f73d9f18516a2bcaab6bbdca73ea54441
oleg <oleg_zub00@mail.ru>
2023-11-08 19:23:20 +0300

    Расширенные товары

--- drfsite/product/admin.py
+++ drfsite/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, SubProduct, Brand, Category, Advantage, Shop, Stock
+from .models import Product, SubProduct, Brand, Category, Advantage, Shop, Stock, Measure
 admin.site.register(Product)
 admin.site.register(SubProduct)
 admin.site.register(Brand)
@@ -7,3 +7,4 @@ admin.site.register(Advantage)
 admin.site.register(Category)
 admin.site.register(Shop)
 admin.site.register(Stock)
+admin.site.register(Measure)
--- drfsite/product/models.py
+++ drfsite/product/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, db_index=True)
     title_dark = models.BooleanField(default=False)
     compound = models.TextField(blank=True)
     description = models.TextField(blank=True)
@@ -13,9 +14,15 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     brand = models.ForeignKey('Brand', on_delete=models.PROTECT, null=True)
-    categories = models.ManyToManyField('Category', blank=True, default=1)
-    advantages = models.ManyToManyField('Advantage', blank=True, default=1)
+    categories = models.ManyToManyField('Category', blank=True)
+    advantages = models.ManyToManyField('Advantage', blank=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('title',)
+        index_together = (('id', 'slug'),)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
     def __str__(self):
         return self.title
@@ -23,8 +30,9 @@ class Product(models.Model):
 
 class SubProduct(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, db_index=True)
     weight_value = models.IntegerField(blank=True)
-    weight_measure = models.CharField(max_length=20)
+    weight_measure = models.ForeignKey('Measure', on_delete=models.CASCADE, null=True)
     images = models.TextField(blank=True)
     price = models.IntegerField(blank=False)
     dimensions_width = models.CharField(max_length=255, blank=True)
@@ -34,18 +42,40 @@ class SubProduct(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    product = models.ForeignKey('Product', on_delete=models.PROTECT, null=True)
-    stock = models.ForeignKey('Stock', on_delete=models.PROTECT, null=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, null=True)
+    stock_item = models.ManyToManyField('Stock', blank=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('title',)
+        index_together = (('id', 'slug'),)
+        verbose_name = 'Подпродукт'
+        verbose_name_plural = 'Подпродукты'
 
     def __str__(self):
         return self.title
+
+
+class Measure(models.Model):
+    value = models.CharField(max_length=25)
+
+    class Meta:
+        verbose_name = 'Мера'
+        verbose_name_plural = 'Меры'
+
+    def __str__(self):
+        return self.value
 
 
 class Brand(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
 
     def __str__(self):
         return self.title
@@ -55,6 +85,11 @@ class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.title
@@ -67,6 +102,11 @@ class Advantage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Преимущество'
+        verbose_name_plural = 'Преимущества'
+
     def __str__(self):
         return self.title
 
@@ -74,24 +114,95 @@ class Advantage(models.Model):
 class Shop(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     address_region = models.CharField(max_length=255, blank=True)
+    address_district = models.CharField(max_length=255, blank=True)
     address_city = models.CharField(max_length=255, blank=True)
     address_street = models.CharField(max_length=255, blank=True)
     address_home = models.CharField(max_length=255, blank=True)
     address_lat = models.CharField(max_length=255, blank=True)
     address_lon = models.CharField(max_length=255, blank=True)
-    address_map_district = models.CharField(max_length=255, blank=True)
     address_map_link = models.TextField(blank=True)
     contact_phone = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
 
     def __str__(self):
         return self.title
 
 
 class Stock(models.Model):
+    title = models.CharField(max_length=255)
     shop = models.ForeignKey('Shop', on_delete=models.PROTECT, null=True)
-    sub_product = models.ForeignKey('SubProduct', related_name="sup_product", on_delete=models.PROTECT, null=True)
-    count = models.IntegerField(blank=True)
+    sub_product = models.ForeignKey('SubProduct', on_delete=models.PROTECT, null=True)
+    count = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
+
+    def __str__(self):
+        return self.title
+
+
+class Cart(models.Model):
+    cart_uuid = models.CharField(max_length=255, unique=True)
+    total_price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+
+
+class CartItem(models.Model):
+    cart_uuid = models.ForeignKey('Cart', to_field='cart_uuid', on_delete=models.PROTECT,
+                                  null=True)
+    order_uuid = models.ForeignKey('Order', to_field='order_uuid', on_delete=models.PROTECT,
+                                   null=True)
+    sub_product = models.ForeignKey('SubProduct', on_delete=models.PROTECT, null=True)
+    count = models.IntegerField(default=1)
+    is_deleted = models.BooleanField(default=False)
+    is_ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Товар в корзине'
+        verbose_name_plural = 'Товары в корзинe'
+
+
+class Order(models.Model):
+    payed = models.BooleanField(default=False)
+    payment_uuid = models.CharField(max_length=255, blank=True, unique=True)
+    order_uuid = models.CharField(max_length=255, unique=True)
+    order_status = models.ForeignKey('OrderStatus', related_name="orderStatus", on_delete=models.PROTECT, null=True,
+                                     default=1)
+    cart_uuid = models.ForeignKey('Cart', to_field='cart_uuid', related_name="cart", on_delete=models.PROTECT,
+                                  null=True)
+    cart_item = models.ForeignKey('CartItem', related_name="cartItem", on_delete=models.PROTECT, null=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class OrderStatus(models.Model):
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Статус заказа'
+        verbose_name_plural = 'Статусы заказов'
+
+    def __str__(self):
+        return self.title
