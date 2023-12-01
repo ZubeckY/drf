fdaec6f99be6697993660a052e24cb353a6cb2a8
oleg <oleg_zub00@mail.ru>
2023-12-01 11:33:34 +0300

    ecosystem args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       [{
         name: "apiLavka",
         script: "drfsite/manage.py",
-        args: ["runserver", "81.200.119.121:8000"],
+        args: ["runserver", "31.129.111.110:8000"],
         exec_mode: "fork",
         instances: "1",
         wait_ready: true,
